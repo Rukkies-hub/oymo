d19b454802180f5e22a3f11627b819d1cfd8d503
Terry Amagboro <rukkiecodes@gmail.com>
2022-08-18 04:31:46 +0100

    add firebase

---
+++ hooks/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } from '@env'
+
+const app = initializeApp({
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
+})
+
+const auth = getAuth()
+const db = getFirestore()
+
+export { auth, db }
