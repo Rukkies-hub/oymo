0f019fbfde328074bdd0fa08e8778eebc51ef3c9
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-03 22:12:44 +0100

    fix chat bug

--- App.js
+++ App.js
@@ -7,10 +7,17 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 
 import { LogBox } from 'react-native'
+import { useFonts } from 'expo-font'
 LogBox.ignoreAllLogs()
 
 
 export default function App () {
+  const [loaded] = useFonts({
+    text: require('./assets/fonts/Montserrat_Alternates/MontserratAlternates-Medium.ttf'),
+    boldText: require('./assets/fonts/Montserrat_Alternates/MontserratAlternates-Bold.ttf')
+  })
+
+  if (!loaded) return null
 
   return (
     <Provider store={store}>
