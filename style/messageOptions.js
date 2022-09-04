8f2e7cb573d13e38d373bada34feb0258f272e16
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-04 18:21:54 +0100

    reply and delte message

---
+++ style/messageOptions.js
@@ -0,0 +1,42 @@
+import { Dimensions, StyleSheet } from "react-native"
+import color from "./color"
+
+const { width } = Dimensions.get('window')
+
+export const mo = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+
+  goBack: {
+    flex: 1,
+    width: '100%'
+  },
+
+  optionsView: {
+    minWidth: width,
+    backgroundColor: color.white,
+    padding: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20
+  },
+
+  replyButton: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: color.offWhite,
+    borderRadius: 12
+  },
+
+  deleteButton: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: color.offWhite,
+    borderRadius: 12,
+    marginTop: 10
+  }
+})
