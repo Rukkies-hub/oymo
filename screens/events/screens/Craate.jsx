33c902d7ad7e5ebdb026565fa02aa1818c494859
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-28 03:16:56 +0100

    add events

--- screens/events/screens/Craate.jsx
+++ screens/events/screens/Craate.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, TouchableOpacity, Image, ScrollView, TextInput, Button, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity, Image, ScrollView, TextInput, Button, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard, Dimensions, ActivityIndicator } from 'react-native'
 import React, { useEffect } from 'react'
 import { create } from '../../../style/events'
 import { useState } from 'react'
@@ -12,26 +12,69 @@ import DateTimePickerModal from "react-native-modal-datetime-picker"
 
 import * as ImagePicker from 'expo-image-picker'
 
+import SelectDropdown from 'react-native-select-dropdown'
+
+import uuid from 'uuid-random'
+import { useSelector } from 'react-redux'
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
+import { doc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore'
+import { db } from '../../../hooks/firebase'
+
+const events = [
+  'Sleep over',
+  'clubbing',
+  'Sit out',
+  'Birthday party',
+  'Pool party',
+  'Wedding',
+  'Convocation',
+  'Matriculation',
+  'Wet shirt party'
+]
+
 const Craate = ({ navigation }) => {
+  const { user, profile } = useSelector(state => state.user)
   const [image, setImage] = useState(null)
   const [title, setTitle] = useState('')
   const [location, setLocation] = useState('')
   const [description, setDescription] = useState('')
   const [mode, setMode] = useState('date')
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false)
+  const [isDurationPickerVisible, setDurationPickerVisibility] = useState(false)
   const [showButton, setShowButton] = useState(true)
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+  const [duration, setDuration] = useState('')
+  const [type, setType] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true)
-  }
+  const storage = getStorage()
 
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false)
-  }
+  let id = user?.uid == undefined ? user?.user?.uid : user?.uid
 
-  const handleConfirm = (date) => {
-    console.warn("A date has been picked: ", date)
+  // date
+  const showDatePicker = () => setDatePickerVisibility(true)
+  const hideDatePicker = () => setDatePickerVisibility(false)
+  const handleDateConfirm = date => {
     hideDatePicker()
+    setDate(JSON.stringify(date))
+  }
+
+  // time
+  const showTimePicker = () => setTimePickerVisibility(true)
+  const hideTimePicker = () => setTimePickerVisibility(false)
+  const handleTimeConfirm = time => {
+    hideTimePicker()
+    setTime(JSON.stringify(time))
+  }
+
+  // duration
+  const showDurationPicker = () => setDurationPickerVisibility(true)
+  const hideDurationPicker = () => setDurationPickerVisibility(false)
+  const handleDurationConfirm = duration => {
+    hideDurationPicker()
+    setDuration(JSON.stringify(duration))
   }
 
   useEffect(() => {
@@ -47,7 +90,6 @@ const Craate = ({ navigation }) => {
   }, [Keyboard])
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -55,11 +97,56 @@ const Craate = ({ navigation }) => {
       aspect: [9, 16]
     })
 
-    console.log(result);
+    if (!result?.cancelled && result?.type == 'image') setImage(result.uri)
+  }
 
-    if (!result.cancelled) {
-      setImage(result.uri)
-    }
+  const saveEvent = async () => {
+    if (profile?.coins <= 0 && image) return
+    if (image && title == '' && location == '' && description == '' && type == '' && date == '' && time == '' && duration == '') return
+
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest()
+      xhr.onload = () => resolve(xhr.response)
+
+      xhr.responseType = 'blob'
+      xhr.open('GET', image, true)
+      xhr.send(null)
+    })
+
+    const link = `events/${id}/${uuid()}`
+
+    const photoRef = ref(storage, link)
+
+    setLoading(true)
+
+    uploadBytes(photoRef, blob)
+      .then(snapshot => {
+        getDownloadURL(snapshot?.ref)
+          .then(async downloadURL => {
+            await setDoc(doc(db, 'events', uuid()), {
+              user: id,
+              image: downloadURL,
+              title,
+              date,
+              time,
+              duration,
+              location,
+              type,
+              description,
+              timestamp: serverTimestamp()
+            })
+            setLoading(false)
+            setImage(null)
+            setTitle('')
+            setDate('')
+            setTime('')
+            setDuration('')
+            setLocation('')
+            setType('')
+            setDescription('')
+            navigation.goBack()
+          })
+      })
   }
 
   return (
@@ -80,8 +167,8 @@ const Craate = ({ navigation }) => {
           }
 
           <BlurView style={create.eventInfoContainer} intensity={50} tint='light'>
-            <OymoFont message={title} fontStyle={create.title} />
-            <OymoFont message={location} fontStyle={create.location} />
+            <OymoFont message={title || 'Title'} fontStyle={create.title} />
+            <OymoFont message={location || 'Location'} fontStyle={create.location} />
           </BlurView>
         </View>
       </ImageBackground>
@@ -92,6 +179,7 @@ const Craate = ({ navigation }) => {
             <TextInput
               value={title}
               onChangeText={setTitle}
+              placeholderTextColor={color.dark}
               placeholder='Event title'
               style={create.input}
             />
@@ -106,7 +194,11 @@ const Craate = ({ navigation }) => {
                   }}
                   style={create.dateInput}
                 >
-                  <OymoFont message='Date' />
+                  {
+                    date == '' ?
+                      <OymoFont message='Date' lines={1} /> :
+                      <OymoFont message={JSON.parse(date) || 'Date'} lines={1} />
+                  }
                 </TouchableOpacity>
               </View>
 
@@ -115,11 +207,15 @@ const Craate = ({ navigation }) => {
                 <TouchableOpacity
                   onPress={() => {
                     setMode('time')
-                    showDatePicker()
+                    showTimePicker()
                   }}
                   style={create.dateInput}
                 >
-                  <OymoFont message='Time' />
+                  {
+                    time == '' ?
+                      <OymoFont message='Time' lines={1} /> :
+                      <OymoFont message={JSON.parse(time) || 'Time'} lines={1} />
+                  }
                 </TouchableOpacity>
               </View>
               <View style={create.dateInputView}>
@@ -127,11 +223,15 @@ const Craate = ({ navigation }) => {
                 <TouchableOpacity
                   onPress={() => {
                     setMode('time')
-                    showDatePicker()
+                    showDurationPicker()
                   }}
                   style={create.dateInput}
                 >
-                  <OymoFont message='Duration' />
+                  {
+                    duration == '' ?
+                      <OymoFont message='Duration' lines={1} /> :
+                      <OymoFont message={JSON.parse(duration) || 'Duration'} lines={1} />
+                  }
                 </TouchableOpacity>
               </View>
             </View>
@@ -139,20 +239,76 @@ const Craate = ({ navigation }) => {
               value={location}
               numberOfLines={1}
               onChangeText={setLocation}
+              placeholderTextColor={color.dark}
               placeholder='Locaation'
               style={create.input}
+            />
+            <SelectDropdown
+              data={events}
+              onSelect={(selectedItem, index) => setType(selectedItem)}
+              buttonTextAfterSelection={(selectedItem, index) => selectedItem}
+              rowTextForSelection={(item, index) => item}
+              buttonStyle={{
+                width: Dimensions.get('window').width - 20,
+                height: 45,
+                borderRadius: 12,
+                marginHorizontal: 10,
+                marginTop: 10,
+                backgroundColor: color.offWhite,
+                padding: 0,
+                flexDirection: 'row',
+                justifyContent: 'flex-start'
+              }}
+              buttonTextStyle={{
+                color: color.dark,
+                fontFamily: 'text',
+                fontSize: 12,
+                textAlign: 'left',
+                marginLeft: 8
+              }}
+              dropdownStyle={{
+                overflow: 'hidden',
+                borderRadius: 12
+              }}
+              dropdownOverlayColor={color.transparent}
+              rowStyle={{
+                backgroundColor: color.white
+              }}
+              rowTextStyle={{
+                textAlign: 'left',
+                marginLeft: 10
+              }}
+              selectedRowStyle={{
+                backgroundColor: color.red
+              }}
+              selectedRowTextStyle={{
+                color: color.white
+              }}
             />
             <TextInput
               value={description}
               onChangeText={setDescription}
+              placeholderTextColor={color.dark}
               placeholder='Event description'
               style={create.input}
             />
             <DateTimePickerModal
-              mode={mode}
-              onConfirm={handleConfirm}
+              mode='date'
+              onConfirm={handleDateConfirm}
               onCancel={hideDatePicker}
               isVisible={isDatePickerVisible}
+            />
+            <DateTimePickerModal
+              mode='time'
+              onConfirm={handleTimeConfirm}
+              onCancel={hideTimePicker}
+              isVisible={isTimePickerVisible}
+            />
+            <DateTimePickerModal
+              mode='time'
+              onConfirm={handleDurationConfirm}
+              onCancel={hideDurationPicker}
+              isVisible={isDurationPickerVisible}
             />
           </ScrollView>
         </TouchableWithoutFeedback>
@@ -161,9 +317,15 @@ const Craate = ({ navigation }) => {
 
       {
         showButton &&
-        <TouchableOpacity style={create.save}>
-          <OymoFont message='Save event' fontStyle={create.saveText} />
-        </TouchableOpacity>
+        <View style={{ backgroundColor: color.white, width: '100%', height: 50 }}>
+          <TouchableOpacity style={create.save} onPress={saveEvent}>
+            {
+              loading ?
+                <ActivityIndicator size='small' color={color.white} /> :
+                <OymoFont message='Save event' fontStyle={create.saveText} />
+            }
+          </TouchableOpacity>
+        </View>
       }
     </View>
   )
