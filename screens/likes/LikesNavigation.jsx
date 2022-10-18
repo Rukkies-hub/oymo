09584e8fb18151a84f8d9fd253a082ea9441d144
Terry Amagboro <rukkiecodes@gmail.com>
2022-10-18 15:19:01 +0100

    fixed likes navigation bug

--- screens/likes/LikesNavigation.jsx
+++ screens/likes/LikesNavigation.jsx
@@ -8,14 +8,46 @@ import { likes } from '../../style/likes'
 import OymoFont from '../../components/OymoFont'
 import color from '../../style/color'
 
-const LikesNavigation = ({ navigation }) => {
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { useNavigation } from '@react-navigation/native'
+const { Navigator, Screen } = createMaterialTopTabNavigator()
+
+const LikesNavigation = () => {
   const { profile, theme } = useSelector(state => state.user)
   const [visible, setVisible] = useState('likes')
 
+  const navigation = useNavigation()
+
+  navigation.addListener('blur', () => {
+    navigation.jumpTo('LikesTab')
+  })
+
   return (
-    <View style={[likes.container, { backgroundColor: theme ? color.dark : color.white }]}>
+    <>
       {
-        !profile ?
+        (profile?.photoURL != undefined && profile?.username != undefined) ?
+          <Navigator
+            screenOptions={{
+              tabBarStyle: {
+                height: 45,
+                elevation: 0,
+                backgroundColor: theme ? color.dark : color.white
+              },
+
+              tabBarIndicatorStyle: {
+                backgroundColor: color.red
+              },
+
+              tabBarLabelStyle: {
+                color: theme ? color.white : color.dark,
+                textTransform: 'capitalize',
+                fontWeight: 'bold'
+              }
+            }}
+          >
+            <Screen name="LikesTab" component={Likes} options={{ tabBarLabel: 'Likes' }} />
+            <Screen name="PassesTab" component={Passes} options={{ tabBarLabel: 'Passes' }} />
+          </Navigator> :
           <View style={likes.setupView}>
             <View style={likes.setupViewSub}>
               <OymoFont message='Creat a profile' fontFamily='montserrat_bold' fontStyle={likes.cp} />
@@ -28,24 +60,9 @@ const LikesNavigation = ({ navigation }) => {
                 <OymoFont message='Create a profile' fontStyle={{ color: color.white }} />
               </TouchableOpacity>
             </View>
-          </View> :
-          <>
-            <View style={[likes.nav, { borderBottomColor: theme ? color.lightBorderColor : color.borderColor }]}>
-              <TouchableOpacity onPress={() => setVisible('likes')} style={likes.navButton}>
-                <OymoFont message='Likes' fontStyle={{ color: visible == 'likes' ? color.red : (theme ? color.white : color.dark) }} />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => setVisible('passes')} style={likes.navButton}>
-                <OymoFont message='Passes' fontStyle={{ color: visible == 'passes' ? color.red : (theme ? color.white : color.dark) }} />
-              </TouchableOpacity>
-            </View>
-
-            {visible == 'likes' && <Likes />}
-
-            {visible == 'passes' && <Passes />}
-          </>
+          </View>
       }
-    </View>
+    </>
   )
 }
 
